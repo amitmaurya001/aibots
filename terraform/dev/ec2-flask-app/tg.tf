805ae058234f8ede9be9030d749ff97dbdd97f15
Amit Maurya <amit@Amits-MacBook-Air.local>
2026-02-27 10:49:32 +0530

    changedheader

--- terraform/dev/ec2-flask-app/tg.tf
+++ terraform/dev/ec2-flask-app/tg.tf
@@ -15,7 +15,7 @@ module "target_group" {
   alb_dns_name     = data.terraform_remote_state.alb.outputs["alb_dns_name"]
 
   host_headers = [
-    "flask.amitwebsite.online"
+    "amitwebsite.online"
   ]
 
   deregistration_delay = 60
